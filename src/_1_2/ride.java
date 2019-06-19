ae8e3baf676b891f0d6b53a375495a3943d12ad3
Coblin123 <52014502+Coblin123@users.noreply.github.com>
2019-06-19 16:38:08 -0700

    start to solve ride
    start to solve ride

---
+++ src/_1_2/ride.java
@@ -0,0 +1,57 @@
+/*
+ * ID : colink91
+ * LANG: JAVA
+ * TASK: gift1
+ */
+package _1_2;
+import java.util.*;
+import java.io.*;
+
+public class ride {	
+	static Scanner in;
+	static PrintWriter out;
+	static String inFile = "ride.in";
+	static String outFile = "ride.out";
+	//static StringBuilder result;
+	static String result = "";
+	//Input Variables
+	static String[] list;
+	
+	public static void main(String args[]) {
+		
+		try {
+			//Initialize in & out
+			in = new Scanner(new File(inFile));
+			out = new PrintWriter(new File(outFile));
+			
+			init();
+			result = solve();
+			
+			//Write out file
+			out.println(result);
+			
+			out.close();
+			in.close();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	} 
+	
+
+	private static void init() {
+		//get data
+		//Initialize Input Variables
+	}
+	private static String solve() {
+		//Process and Output
+		return result;
+
+	}
+	// Algorithm methods
+//	private static int change(String s){
+//
+//
+//}
+}
