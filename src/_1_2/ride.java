b3302f5062b3363818a5cf54e4fc5d58454c59a0
Coblin123 <52014502+Coblin123@users.noreply.github.com>
2019-06-20 14:44:56 -0700

    Finish to solve Ride problem
    Solved

--- src/_1_2/ride.java
+++ src/_1_2/ride.java
@@ -1,7 +1,7 @@
 /*
- * ID : colink91
- * LANG: JAVA
- * TASK: ride
+ID : colink91
+LANG: JAVA
+TASK: ride
  */
 package _1_2;
 import java.util.*;
@@ -16,6 +16,10 @@ public class ride {
 	static String result = "";
 	//Input Variables
 	static String[] list;
+	static String input;
+	static String input2; 
+	static int word1;
+	static int word2;
 	
 	public static void main(String args[]) {
 		
@@ -43,9 +47,26 @@ public class ride {
 	private static void init() {
 		//get data
 		//Initialize Input Variables
+		input = in.nextLine();
+		input2 = in.nextLine();
+		word1 = 1;
+		word2 = 1;
+		
 	}
 	private static String solve() {
 		//Process and Output
+		for(int i = 0; i < input.length(); i++) {
+			word1 *= (int)input.charAt(i) - 64;
+		}
+		for(int i = 0; i < input2.length(); i++) {
+			word2 *= (int)input2.charAt(i) - 64;
+		}
+
+		if(word1% 47  == word2%47) {
+			result = "GO";
+		}else {
+			result = "STAY";
+		}
 		return result;
 
 	}
