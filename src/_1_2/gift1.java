05ba95a99165e6d3b956c386d44b454043bcabbf
Coblin123 <52014502+Coblin123@users.noreply.github.com>
2019-06-20 16:37:05 -0700

    Solved gift1
    Solved gift1

--- src/_1_2/gift1.java
+++ src/_1_2/gift1.java
@@ -1,7 +1,7 @@
 /*
- * ID : colink91
- * LANG: JAVA
- * TASK: gift1
+ID : colink91
+LANG: JAVA
+TASK: gift1
  */
 package _1_2;
 import java.util.*;
@@ -17,6 +17,9 @@ public class gift1 {
 	static String result = "";
 	//Input Variables
 	static String[] list;
+	static LinkedHashMap<String, Integer> accounts;
+	static int give;
+	static int NP;
 	
 	public static void main(String args[]) {
 		
@@ -29,7 +32,7 @@ public class gift1 {
 			result = solve();
 			
 			//Write out file
-			out.println(result);
+			out.print(result);
 			
 			out.close();
 			in.close();
@@ -44,12 +47,41 @@ public class gift1 {
 	private static void init() {
 		//get data
 		//Initialize Input Variables
-		
+		NP = Integer.parseInt(in.nextLine());
+		accounts = new LinkedHashMap<String, Integer>();
+		for(int i = 0; i < NP; i++) {
+			accounts.put(in.nextLine(), 0);
+		}
 	}
 	private static String solve() {
 		//Process and Output
+		for(int i = 0; i < NP; i++) {
+			String giver = in.nextLine();
+			String[] money = in.nextLine().split(" ");
+			System.out.println(Arrays.toString(money));
+			int totalG = Integer.parseInt(money[0]);
+			int gifted = Integer.parseInt(money[1]);
+			if(gifted == 0) {
+				continue;
+			}
+			int moneyL = totalG % gifted;
+			int moneyG = totalG / gifted;
+			int temp = (accounts.get(giver) - Integer.parseInt(money[0])) + moneyL; 
+			accounts.put(giver, temp);
+			temp = 0;
+			
+			for(int j = 0; j < gifted; j++) {
+				String reciever = in.nextLine();
+				temp = accounts.get(reciever) + moneyG;
+				accounts.put(reciever, temp);
+			}
+			
+		}
+		for(String s: accounts.keySet()) {
+			result += s + " " + accounts.get(s) + "\n";
+		}
 		return result;
-
+		
 	}
 	// Algorithm methods
 //	private static int change(String s){
