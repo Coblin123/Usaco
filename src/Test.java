bb8c44a7f1da2f928fd81250fa90ccc73551029b
Coblin123 <52014502+Coblin123@users.noreply.github.com>
2019-06-20 14:44:36 -0700

    Create test Class
    For testing

---
+++ src/Test.java
@@ -0,0 +1,27 @@
+
+public class Test {
+	public static void main(String[] args) {
+		
+		int a = 67;
+		System.out.println(a);
+		
+		char b = 'C';
+		System.out.println(b);
+		
+		System.out.println((char)a);
+		System.out.println((int)b);
+		
+		String str = "CD";
+		int result = 1;
+		
+		for(int i = 0; i < str.length(); i++) {
+			
+			char ch = str.charAt(i);
+			System.out.println((int)ch);
+			result *= (int)ch;
+		}
+		
+		System.out.println(result);
+		
+	}
+}
