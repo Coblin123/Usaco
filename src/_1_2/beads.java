ab596522466e5ad9b333a36dec27108f1db5e5a0
Coblin123 <52014502+Coblin123@users.noreply.github.com>
2019-06-27 16:49:29 -0700

    finished beads
    finished beads

--- src/_1_2/beads.java
+++ src/_1_2/beads.java
@@ -1,7 +1,7 @@
 /*
 ID : colink91
 LANG: JAVA
-TASK: gift1
+TASK: beads
  */
 package _1_2;
 import java.util.*;
@@ -14,19 +14,22 @@ public class beads {
 
     //Input Variables
     static String [] list;
+    static String input;
+    static String temp;
+    static int length;
 
     public static void main(String[] args) {
 
         try {
 
             //Initialize in & out
-            in = new Scanner(new File("xxx.in"));
-            out = new PrintWriter(new File("xxx.out"));
+            in = new Scanner(new File("beads.in"));
+            out = new PrintWriter(new File("beads.out"));
 
             init();
 
             //Write out file
-            out.print(solve());
+            out.println(solve());
 
             out.close();
             in.close();
@@ -42,19 +45,62 @@ public class beads {
     private static void init() {
         //Get data 
         //Initialize Input Variables
-
-
-
+    	temp= in.nextLine();
+    	input = in.nextLine();
+    	length = Integer.parseInt(temp);
+    	
+    	
     }
 
 
-    private static String solve() {
-        String result = "";
-
-
+    private static int solve() {
+    	int result = 0;
+        int ans = 0;
+        input += input;
+       
+    	for(int i = 0; i < input.length()/2; i++) {
+    		int c = 1;
+    		char start = input.charAt(i);
+    		
+    		if(start == 'w') {
+    			c = Math.max(recur(i, 'b'), recur(i, 'r'));
+    		}else {
+    			c = recur(i, start);
+    			
+    		}
+    		if(ans < c) {
+				ans = c;
+			}
+    	}
+        result = ans;
 
         return result;
 
+    }
+    private static int recur(int i, char start) {
+    	boolean change = false;
+    	int indx = i+1;
+    	int count = 1;
+        
+    	while (count < input.length()/2) {
+    		char curr = input.charAt(indx);
+    		if(!change) {
+    			if(curr == start || curr == 'w') {
+    				count++;
+    			}else {
+    				change = true;
+    				count++;
+    			}
+    		}else {
+    			if(curr != start || curr == 'w') {
+    				count++;
+    			}else {
+    				break;
+    			}
+    		}
+    		indx++;
+    	}
+        return count;
     }
 
     //Algorithm methods
