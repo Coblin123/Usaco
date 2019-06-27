6f8495a6afb0d972451e113fc5ef11447eac584c
Coblin123 <52014502+Coblin123@users.noreply.github.com>
2019-06-27 16:50:57 -0700

    Changed the template
    changed the template

--- src/template.java
+++ src/template.java
@@ -6,51 +6,61 @@ TASK: gift1
 import java.util.*;
 import java.io.*;
 
-public class template {	
-	static Scanner in;
-	static PrintWriter out;
-	static String inFile = "xxx.in";
-	static String outFile = "xxx.out";
-	//static StringBuilder result;
-	static String result = "";
-	//Input Variables
-	static String[] list;
-	
-	public static void main(String args[]) {
-		
-		try {
-			//Initialize in & out
-			in = new Scanner(new File(inFile));
-			out = new PrintWriter(new File(outFile));
-			
-			init();
-			result = solve();
-			
-			//Write out file
-			out.print(result);
-			
-			out.close();
-			in.close();
-			
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-		
-	} 
-	
+public class template {
 
-	private static void init() {
-		//get data
-		//Initialize Input Variables
-	}
-	private static String solve() {
-		//Process and Output
-		return result;
+    static Scanner in;
+    static PrintWriter out;
 
-	}
-	// Algorithm methods
-//	private static int change(String s){
+    //Input Variables
+    static String [] list;
+
+    public static void main(String[] args) {
+
+        try {
+
+            //Initialize in & out
+            in = new Scanner(new File("xxx.in"));
+            out = new PrintWriter(new File("xxx.out"));
+
+            init();
+
+            //Write out file
+            out.print(solve());
+
+            out.close();
+            in.close();
+
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+    private static void init() {
+        //Get data 
+        //Initialize Input Variables
+
+
+
+    }
+
+
+    private static String solve() {
+        String result = "";
+
+
+
+        return result;
+
+    }
+
+    //Algorithm methods
+//    private static int change(String s) {
 //
 //
-//}
+//    }
+
+
 }
