6e4d772a2a057f8d00937a10b5c6ae5fed3f0f1a
Coblin123 <52014502+Coblin123@users.noreply.github.com>
2019-08-08 16:04:50 -0700

    Finished the question milk2
    finished milk2

--- src/_1_3/milk2.java
+++ src/_1_3/milk2.java
@@ -13,20 +13,35 @@ public class milk2 {
     static PrintWriter out;
 
     //Input Variables
-    static String [] list;
+    static int N;
+    static cow[] list;
+    
+    static class cow implements Comparable<cow>{
+    	int start;
+    	int end;
+    	public cow(int s, int e) {
+    		start = s;
+    		end = e;
+    	}
 
+		@Override
+		public int compareTo(cow arg0) {
+			return this.start - arg0.start;
+		}
+    }
+    
     public static void main(String[] args) {
 
         try {
 
             //Initialize in & out
-            in = new Scanner(new File("milk.in"));
-            out = new PrintWriter(new File("milk.out"));
+            in = new Scanner(new File("milk2.in"));
+            out = new PrintWriter(new File("milk2.out"));
 
             init();
 
             //Write out file
-            out.print(solve());
+            out.println(solve());
 
             out.close();
             in.close();
@@ -42,18 +57,57 @@ public class milk2 {
     private static void init() {
         //Get data 
         //Initialize Input Variables
-
-
-
+    	N  = in.nextInt();
+    	list = new cow[N];
+    	for(int i = 0; i < list.length; i++) {
+    		int start = in.nextInt();
+    		int end = in.nextInt();
+    		list[i] = new cow(start, end);
+    
+    	}
+    	Arrays.sort(list);
     }
 
 
     private static String solve() {
-        String result = "";
-
-
-
-        return result;
+    	int start = list[0].start;
+    	int end = list[0].end;
+    	int discontinued = 0;
+    	int chain = 0;
+    	int mDiscontinued = 0;
+    	int mChain = 0;
+    	
+    	    	
+    	for(int j = 0; j < list.length; j++) {
+    		int currStart = list[j].start;
+    		int currEnd = list[j].end;
+    		if(currEnd <= end) {
+    			continue;
+    		}else if(currStart <= end) {
+    			end = currEnd;
+    		}else {
+    			discontinued = currStart - end;
+    			if(discontinued > mDiscontinued) {
+    				mDiscontinued = discontinued;
+    			}
+    			chain = end - start;
+    			if(chain > mChain) {
+    				mChain = chain;
+    			}
+    			start = currStart;
+    			end = currEnd;
+    		} 
+    	}
+    	
+    	int conti = end - start;
+    	if(conti > mChain) {
+    		mChain = conti;
+    	}
+    	
+    	/*for(int i = 0; i < list.length;i++) {
+    		System.out.println(list[i].start + " " + list[i].end);
+    	}*/
+        return mChain + " " + mDiscontinued;
 
     }
 
